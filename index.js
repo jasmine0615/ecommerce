d7899ad151322b15f1caf2e28f00868c474aeabd
jasmine0615 <101412448+jasmine0615@users.noreply.github.com>
2022-08-28 12:21:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
 if (bar) {
-  bar.addEventListener("click", () => {
+  bar.addEventListener(onclick, () => {
     nav.classList.add("active");
   });
 }
 if (close) {
-  bar.addEventListener("click", () => {
+  bar.addEventListener(onclick, () => {
     nav.classList.remove("active");
   });
 }
